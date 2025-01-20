e4e9fd3b66c5515ec1eab80ffd28b5c194111092
Unska-Winter <3868985204@qq.com>
2025-01-20 21:28:55 +0800

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,1 +1,6 @@
-print('hello world!')
+def trans_str_to_numberlist(str,n):
+  str_split = str.split()
+  number_list = []
+  for i in range(0,n):
+    number_list.append(int(str_split[i]))
+  return number_list
